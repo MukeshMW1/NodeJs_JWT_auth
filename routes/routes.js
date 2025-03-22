db5588889971a395b738791db58435149b1301c7
MukeshMW1 <MukeshBhattaMPB111@gmail.com>
2025-03-22 14:28:52 +0545

    routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,25 @@
 const app = require('../main')
 
-const router = app.router()
+const router = (req,res)=>{
+res.setHeader('Content-Type','application/json')
+
+if(req.url === '/signup' && req.method === 'POST'){
+
+}
+
+if(req.url === '/login' && req.method === 'POST' ){
+
+}
+
+
+
+if(req.url === '/profile' && req.method === 'GET'){
+
+}
+
+
+if(req.url === '/admin' && req.method === 'GET'){
+    
+}
+
+}
