d1a2eb87c2f53e15800ccad8d4dcdfcad7064c67
MukeshMW1 <MukeshBhattaMPB111@gmail.com>
2025-03-28 15:34:29 +0545

    updates

--- routes/routes.js
+++ routes/routes.js
@@ -4,22 +4,25 @@ const router = (req,res)=>{
 res.setHeader('Content-Type','application/json')
 
 if(req.url === '/signup' && req.method === 'POST'){
-
+console.log('Sign Up')
 }
 
 if(req.url === '/login' && req.method === 'POST' ){
+    console.log('Login')
 
 }
 
 
 
 if(req.url === '/profile' && req.method === 'GET'){
+    console.log('Profile')
 
 }
 
 
 if(req.url === '/admin' && req.method === 'GET'){
     
+console.log('Admin')
 }
 
 }
